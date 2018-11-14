15f26e9ffb701224162c1aeb492445ddabebd8d2
sleeplesseditor <josh@bryson.me.uk>
2018-11-14 01:03:35 +0000

    Initial route setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+import App from './components/App';
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
+
+const Root = () => (
+    <Router>
+        <Switch>
+            <Route exact path="/" component={App} />
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+        </Switch>
+    </Router>
+);
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
