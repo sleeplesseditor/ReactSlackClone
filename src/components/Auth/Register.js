ecb836fc84fef219b816065484e5b1e24729497a
sleeplesseditor <josh@bryson.me.uk>
2018-11-14 12:12:48 +0000

    User registration setup

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,17 +1,38 @@
 import React, { Component } from 'react';
+import firebase from '../../firebase';
 import { Grid, Form, Segment, Button, Header, Message, Icon } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
 class Register extends Component {
     state = {
-
+        username: '',
+        email: '',
+        password: '',
+        passwordConfirmation: ''
     };
 
-    handleChange = () => {
+    handleChange = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
 
+    handleSubmit = event => {
+        event.preventDefault();
+        firebase
+            .auth()
+            .createUserWithEmailAndPassword(this.state.email, this.state.password)
+            .then(createdUser => {
+                console.log(createdUser);
+            })
+            .catch(err => {
+                console.error(err);
+            })
     }
 
     render() {
+        const { username, email, password, passwordConfirmation } = this.state;
+
         return (
             <Grid textAlign="center" verticalAlign="middle" className="app">
                 <Grid.Column style={{ maxWidth: 450 }}>
@@ -19,7 +40,7 @@ class Register extends Component {
                         <Icon name="puzzle piece" color="orange"/>
                         Register for DevChat
                     </Header>
-                    <Form size="large">
+                    <Form onSubmit={this.handleSubmit} size="large">
                         <Segment stacked>
                             <Form.Input 
                                 fluid name="username" 
@@ -27,6 +48,7 @@ class Register extends Component {
                                 iconPosition="left" 
                                 placeholder="Username" 
                                 onChange={this.handleChange}
+                                value={username}
                                 type="text"
                             />
                             <Form.Input 
@@ -35,6 +57,7 @@ class Register extends Component {
                                 iconPosition="left" 
                                 placeholder="Email Address" 
                                 onChange={this.handleChange}
+                                value={email}
                                 type="email"
                             />
                             <Form.Input 
@@ -43,6 +66,7 @@ class Register extends Component {
                                 iconPosition="left" 
                                 placeholder="Password" 
                                 onChange={this.handleChange}
+                                value={password}
                                 type="password"
                             />
                             <Form.Input 
@@ -51,6 +75,7 @@ class Register extends Component {
                                 iconPosition="left" 
                                 placeholder="Confirm Password" 
                                 onChange={this.handleChange}
+                                value={passwordConfirmation}
                                 type="password"
                             />
                             <Button 
