5abd2df532637ea280143f5acfa8e5629eff0d90
sleeplesseditor <josh@bryson.me.uk>
2018-11-17 15:40:38 +0000

    OnAuthStateChanged setup for login

--- src/index.js
+++ src/index.js
@@ -1,22 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
-import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route, withRouter } from 'react-router-dom';
 
 import 'semantic-ui-css/semantic.min.css';
 import App from './components/App';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
+import firebase from './firebase';
 
-const Root = () => (
+class Root extends Component {
+    componentDidMount(){
+        firebase.auth().onAuthStateChanged(user => {
+            if(user){
+                this.props.history.push('/');
+            }
+        })
+    }
+
+    render() {
+        return (
+            <Switch>
+                <Route exact path="/" component={App} />
+                <Route path="/login" component={Login} />
+                <Route path="/register" component={Register} />
+            </Switch>
+        );
+    }
+};
+
+const RootWithAuth = withRouter(Root);
+
+ReactDOM.render(
     <Router>
-        <Switch>
-            <Route exact path="/" component={App} />
-            <Route path="/login" component={Login} />
-            <Route path="/register" component={Register} />
-        </Switch>
-    </Router>
-);
-
-ReactDOM.render(<Root />, document.getElementById('root'));
+        <RootWithAuth />
+    </Router>, document.getElementById('root')
+    );
 registerServiceWorker();
