c03c3eb00a7433fea23efa83f752a8c5bbd899d9
sleeplesseditor <josh@bryson.me.uk>
2018-11-17 15:59:16 +0000

    Loading spinner setup

--- src/index.js
+++ src/index.js
@@ -2,24 +2,33 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import {BrowserRouter as Router, Switch, Route, withRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider, connect } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import 'semantic-ui-css/semantic.min.css';
 import App from './components/App';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
+import Spinner from './Spinner';
 import firebase from './firebase';
+import rootReducer from './reducers';
+import { setUser } from './actions';
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 class Root extends Component {
     componentDidMount(){
         firebase.auth().onAuthStateChanged(user => {
             if(user){
+                this.props.setUser(user);
                 this.props.history.push('/');
             }
         })
     }
 
     render() {
-        return (
+        return this.props.isLoading ? <Spinner/> : (
             <Switch>
                 <Route exact path="/" component={App} />
                 <Route path="/login" component={Login} />
@@ -29,11 +38,23 @@ class Root extends Component {
     }
 };
 
-const RootWithAuth = withRouter(Root);
+const mapStateToProps = state => ({
+    isLoading: state.user.isLoading
+});
+
+const RootWithAuth = withRouter(
+    connect(
+        mapStateToProps, 
+        { setUser }
+    )(Root)
+);
 
 ReactDOM.render(
-    <Router>
-        <RootWithAuth />
-    </Router>, document.getElementById('root')
+    <Provider store={store}>
+        <Router>
+            <RootWithAuth />
+        </Router>
+    </Provider>
+    , document.getElementById('root')
     );
 registerServiceWorker();
