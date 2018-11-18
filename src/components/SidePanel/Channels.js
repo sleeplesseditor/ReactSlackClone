f7700f93a7d891b1fde21f76fb67b49e3183ce6b
sleeplesseditor <josh@bryson.me.uk>
2018-11-18 01:36:36 +0000

    Channel select functionality added

--- src/components/SidePanel/Channels.js
+++ src/components/SidePanel/Channels.js
@@ -1,28 +1,51 @@
 import React, { Component } from 'react';
 import firebase from '../../firebase';
+import { connect } from 'react-redux';
+import { setCurrentChannel } from '../../actions';
 import { Button, Form, Icon, Input, Menu, Modal } from 'semantic-ui-react';
 
 class Channels extends Component {
     state = {
+        activeChannel: '',
         user: this.props.currentUser,
         channels: [],
         channelName: '',
         channelDetails: '',
         channelsRef: firebase.database().ref('channels'),
-        modal: false
+        modal: false,
+        firstLoad: true
     }
 
     componentDidMount(){
         this.addListeners();
     }
 
+    componentWillMount(){
+        this.removeListeners();
+    }
+
     addListeners = () => {
         let loadedChannels = [];
         this.state.channelsRef.on('child_added', snap => {
             loadedChannels.push(snap.val());
-            this.setState({ channels: loadedChannels });
+            this.setState({ channels: loadedChannels }, () => this.setFirstChannel());
         })
+    };
+
+    removeListeners = () => {
+        this.state.channelsRef.off();
     }
+
+    setFirstChannel = () => {
+        const firstChannel = this.state.channels[0];
+        if(this.state.firstLoad && this.state.channels.length > 0) {
+            this.props.setCurrentChannel(firstChannel);
+            this.setActiveChannel(firstChannel);
+        }
+        this.setState({ 
+            firstLoad: false 
+        });
+    };
 
     addChannel = () => {
         const { channelsRef, channelName, channelDetails, user } = this.state;
@@ -67,13 +90,25 @@ class Channels extends Component {
         });
     }
 
+    changeChannel = channel => {
+        this.setActiveChannel(channel);
+        this.props.setCurrentChannel(channel);
+    }
+
+    setActiveChannel = channel => {
+        this.setState({
+            activeChannel: channel.id
+        });
+    }
+
     displayChannels = channels => (
         channels.length > 0 && channels.map(channel => (
             <Menu.Item
                 key={channel.id}
-                onClick={() => console.log(channel)}
+                onClick={() => this.changeChannel(channel)}
                 name={channel.name}
                 style={{ opacity: 0.7 }}
+                active={channel.id === this.state.activeChannel}
             >
                 # {channel.name}
             </Menu.Item>
@@ -148,4 +183,7 @@ class Channels extends Component {
     }
 }
 
-export default Channels;
+export default connect(
+    null, 
+    { setCurrentChannel }
+)(Channels);
