1ccb0f7678e6597d1a46186899338dab95bc63ad
sleeplesseditor <josh@bryson.me.uk>
2018-11-18 12:17:42 +0000

    Display messages functionality added

--- src/components/App.js
+++ src/components/App.js
@@ -9,13 +9,20 @@ import MetaPanel from './MetaPanel/MetaPanel';
 
 import './App.css';
 
-const App = ({ currentUser }) => (
+const App = ({ currentUser, currentChannel }) => (
   <Grid columns="equal" className="app" style={{ background: "#eee" }}>
     <ColorPanel />
-    <SidePanel currentUser={currentUser}/>
+    <SidePanel 
+      key={currentUser && currentUser.uid}
+      currentUser={currentUser}
+    />
     
     <Grid.Column style={{ marginLeft: 320 }}>
-      <Messages />
+      <Messages 
+        key={currentChannel && currentChannel.id}
+        currentChannel={currentChannel}
+        currentUser={currentUser}
+      />
     </Grid.Column>
     
     <Grid.Column width={4}>
@@ -25,7 +32,8 @@ const App = ({ currentUser }) => (
 )
 
 const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+  currentUser: state.user.currentUser,
+  currentChannel: state.channel.currentChannel
 });
 
 export default connect(mapStateToProps)(App);
