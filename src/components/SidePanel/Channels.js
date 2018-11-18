a441906331a52c8c36ffd432a307de6d3df2a591
sleeplesseditor <josh@bryson.me.uk>
2018-11-18 00:49:46 +0000

    Channel Display added to Side Panel

--- src/components/SidePanel/Channels.js
+++ src/components/SidePanel/Channels.js
@@ -12,6 +12,18 @@ class Channels extends Component {
         modal: false
     }
 
+    componentDidMount(){
+        this.addListeners();
+    }
+
+    addListeners = () => {
+        let loadedChannels = [];
+        this.state.channelsRef.on('child_added', snap => {
+            loadedChannels.push(snap.val());
+            this.setState({ channels: loadedChannels });
+        })
+    }
+
     addChannel = () => {
         const { channelsRef, channelName, channelDetails, user } = this.state;
         const key = channelsRef.push().key;
@@ -55,6 +67,19 @@ class Channels extends Component {
         });
     }
 
+    displayChannels = channels => (
+        channels.length > 0 && channels.map(channel => (
+            <Menu.Item
+                key={channel.id}
+                onClick={() => console.log(channel)}
+                name={channel.name}
+                style={{ opacity: 0.7 }}
+            >
+                # {channel.name}
+            </Menu.Item>
+        ))
+    )
+
     isFormValid = ({ channelName, channelDetails }) => channelName && channelDetails;
 
     openModal = () => this.setState({
@@ -77,7 +102,7 @@ class Channels extends Component {
                         </span>{' '}
                         ({ channels.length }) <Icon name="add" onClick={this.openModal}/>
                     </Menu.Item>
-                    {/* Channels */}
+                    {this.displayChannels(channels)}
                 </Menu.Menu>
 
                 {/*Add Channel Modal*/}
