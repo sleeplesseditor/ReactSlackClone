2fc57e2ed53fd482f4c4e870ea10ebbf4a431131
sleeplesseditor <josh@bryson.me.uk>
2018-11-21 11:54:33 +0000

    Favouriting system updated

--- src/components/Messages/MessagesHeader.js
+++ src/components/Messages/MessagesHeader.js
@@ -8,7 +8,9 @@ class MessagesHeader extends Component {
             numUniqueUsers, 
             handleSearchChange, 
             searchLoading,
-            isPrivateChannel 
+            isPrivateChannel,
+            handleStar,
+            isChannelStarred 
         } = this.props;
 
         return(
@@ -22,7 +24,13 @@ class MessagesHeader extends Component {
                 >
                     <span>
                         {channelName}
-                        {!isPrivateChannel && <Icon name={'star outline'} color="black" />}
+                        {!isPrivateChannel && (
+                            <Icon 
+                                onClick={handleStar} 
+                                name={isChannelStarred ? 'star': 'star outline'} 
+                                color={isChannelStarred ? 'yellow' : 'black'}
+                            />
+                        )}
                     </span>
                     <Header.Subheader>{numUniqueUsers}</Header.Subheader>
                 </Header>
--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -1,6 +1,7 @@
 import React, { Component} from 'react';
 import { Menu } from 'semantic-ui-react';
 import UserPanel from './UserPanel';
+import Starred from './Starred';
 import Channels from './Channels';
 import DirectMessages from './DirectMessages';
 
@@ -17,6 +18,7 @@ class SidePanel extends Component {
                 style={{ background: '#4c3c4c', fontSize: "1.2rem" }}
             >
                 <UserPanel currentUser={currentUser} />
+                <Starred currentUser={currentUser} />
                 <Channels currentUser={currentUser} />
                 <DirectMessages currentUser={currentUser} />
             </Menu>
